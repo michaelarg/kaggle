620b97ffaacd3c3ead4f74424e1218f9a94392af
Michael Argyrides <michael.argyrides@readify.net>
2018-02-20 13:06:24 +1100

    updated with edge cases and zero edge bug fixed

--- nucleus /python_scripts/pixeltoform.py
+++ nucleus /python_scripts/pixeltoform.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 #os.chdir("/Users/michaelargyrides/Documents/datascience_nuclei/stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/images")
 #img = cv2.imread('0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9.png',0) #the image read in is a grayscale image
 
-''' **this script isn't working at the moment
+'''
 This script is written for the kaggle competition of classifying nuclei.
 They have a particular format for the submission scoring. Ex. 32 3 means
 start at pixel 32 and then 33 and 34.
@@ -20,139 +20,145 @@ To check that this is correct I will then convert the submission form back
 into a matrix form to check that it is correct by printing the matrix.
 '''
 
+np.set_printoptions(threshold=np.nan)
+def to_form(img):
+    a = np.nonzero(img) #find all the indices that are non zero in the image
 
-os.chdir("/Users/michaelargyrides/Documents/datascience_nuclei/stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks")
-img = cv2.imread('8246970b0d108736b3e8a1a45872737a30a2332dc00fce23534c86b1cbc80f73.png',0) #the image read in is a grayscale image
-
-#print(glob.glob("/*.png"))
-
-for filename in glob.glob('/Users/michaelargyrides/Documents/datascience_nuclei/stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks*.png'):
-    print filename
-
-print img.shape
-
-a = np.nonzero(img)
-b = zip(a[1]*256+1,a[0])
-c = [i[0] + i[1] for i in b]
-c = sorted(c)
-
-#cv2.imshow('image',img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-
-def conseq_count(list):
+    a = zip(a[1], a[0])
+    a = sorted(a)
+    a = [list(i) for i in a]
+    b = [i[0]*img.shape[0]+1+i[1] for i in a]
+    
     count_list = []
     novals = []
-    novals.append(list[0])
+    novals.append(b[0]) #the first value is put in
+    
+    novals.pop(0)
+    
+  #  print novals
+ #   print 'length of list' , len(b)
     count = 1
-    for i in range(len(list)-1):
-        if list[i] + 1 == list[i+1]:
+    for i in range(len(b)-1):
+        if b[i] + 1 == b[i+1]:
             if count == 1:
-                novals.append(list[i])
+                novals.append(b[i])
             count +=1
         else:
             count_list.append(count)
             count = 1
+
+    if (b[len(b)-1]-1) != b[len(b)-2]:
+        print "they are not consecutive bra"
+        novals.append(b[len(b)-1])
+        count_list.append(1)
+          
     count_list.append(count)
-    print "here!" , len(novals)
-    return zip(novals, count_list)
+
+    kk = zip(novals, count_list)
+
+    kk = re.sub(r'[^\w]', ' ', str(kk))      
+    kk = " ".join(kk.split())
+
+    return kk
 
 
-dd = conseq_count(c)
+def from_formtest(subform, img):
+    subform = subform.split()
+ #   print subform
+    k = np.array(subform)
+    k= k.astype(int)
 
-dd = re.sub(r'[^\w]', ' ', str(dd))      
-dd = " ".join(dd.split())
+    masks = k.reshape([len(subform)/2,2])
 
-print dd
+    master = []
+    for i in range(len(masks)):
+        sub = []
+        for j in range(masks[i][1]):
+            sub.append(masks[i][0] + j)
+        master.append(sub)
 
-#print dd
-dd = dd.split()
-k = np.array(dd)
-k= k.astype(int)
-masks = k.reshape([len(dd)/2,2])
+    C = [item for sublist in master for item in sublist] #very efficient list operations!
+    Z = np.repeat(1,len(C))
+    er = zip(C,Z)
 
+    blank = [range(0,np.shape(img)[0] * np.shape(img)[0])] #big list! index
+    blank = [item for sublist in blank for item in sublist]
 
-master = []
-for i in range(len(masks)):
-    sub = []
-    for j in range(masks[i][1]):
-        sub.append(masks[i][0] + j)
-    master.append(sub)
+    zilch = [np.repeat(0, np.shape(img)[0] * np.shape(img)[0] )] #zeros
+    zilch = [item for sublist in zilch for item in sublist]
 
+    dds = zip(blank, zilch )
 
-C = [item for sublist in master for item in sublist] #very efficient list operations!
-Z = np.repeat(1,len(C))
+    dds = [list(elem) for elem in dds]
+    er = [list(elem) for elem in er] #tuples to lists
+    pwq = [list(e) for e in zip([blank,zilch])] #big list of 256*256 values all zero now
 
-er = zip(C,Z)
+    out_tup = [item for item in er if item[1] not in dds[0] ] #outtup is a list of each pixel from sub form and a 1
 
+    countit=0
+    for i in er:    #print i[0]
+       # print i[0]-1
+      
+        dds[i[0]-1][1] = 255
+        countit += 1
 
-blank = [range(0,np.shape(img)[0] * np.shape(img)[0]         )]
-blank = [item for sublist in blank for item in sublist]
+    pixelvals = [index[1] for index in dds]
 
-zilch =      [np.repeat(0, np.shape(img)[0] * np.shape(img)[0] )]
-zilch = [item for sublist in zilch for item in sublist]
-
-dds = zip(blank, zilch )
-
-dds = [list(elem) for elem in dds]
-er = [list(elem) for elem in er]
-
-pwq = [list(e) for e in zip([blank,zilch])]
-
-out_tup = [item for item in er if item[1] not in dds[0] ]
-
-for i in er:    #print i[0]
-    dds[i[0]][1] = 255
-
-pixelvals = [index[1] for index in dds]
-
-pixel_shape = np.reshape(pixelvals, [np.shape(img)[0],np.shape(img)[0]])
-
-plt.imshow(pixel_shape)
-plt.gray()
-plt.show()
-
-#rew = [dd
-
-#    dds = del dds(i)
- #   dds.append(er[i])
-    
-    
-#    print er[i] in dds
-   # if er[i] in dds == True:
-        
-        #delete current value 'no' and replace with 'yes'
-        
-#if any(er[0][0] == dds[i][0] for i in range(0,len(dds))):
-#    print "ya"
+    pixel_shape = np.reshape(pixelvals, [np.shape(img)[0],np.shape(img)[0]])
+    test = pixel_shape.T
+    return test
 
 
 
+def plot_test(test, real):
+    print "are plots equal", np.array_equal(test,real)
+    print np.count_nonzero(test)
+    print np.count_nonzero(real)
 
-#er is the pixels
-#dds is the blank pixels
+    f = plt.figure()
+    f.add_subplot(1,2,1)
+    plt.gray()
+    plt.imshow(test)
+    f.add_subplot(1,2,2)
+    plt.imshow(real)
+    plt.title("actual")
+    plt.gray()
+    plt.show(block=True)
 
 
-'''
-flatten a black image and overwrite the pixels that correspond to the values in C
+#print np.array_equal(img, test)
+
+def main():
+    os.chdir("/Users/michaelargyrides/Documents/datascience_nuclei/stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks")
+    img = cv2.imread('39acbc082528c996d9fc3a3181938580f6c48461ee9eea91ff273e9d2c4499d0.png',0) #the image read in is a grayscale image
+
+    subform = to_form(img)
+    print subform
+    coolmat = from_formtest(subform, img)
 
 
-'''
+    plot_test(coolmat, img)
+
+
+if __name__ == '__main__':
+    main()
 
 
 
 
-#pixel_mask = 
 
-#indexpixel = np.array(dd)
 
-#print b
 
-#print a[1]
 
-#counter=collections.Counter(a[1])
-#print counter
 
-#print (np.nonzero(img)[1]) #np.nonzero returns a tuple indicating the x,y coords of non zero pixels in the image.
+
+
+
+
+
+
+
+
+
+
 
