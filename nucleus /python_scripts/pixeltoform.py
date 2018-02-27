77d353fe9c36215853cc5036a60836b14d9e4f33
Michael Argyrides <michael.argyrides@readify.net>
2018-02-27 15:25:37 +1100

    work for non square matrices

--- nucleus /python_scripts/pixeltoform.py
+++ nucleus /python_scripts/pixeltoform.py
@@ -7,7 +7,8 @@ import re
 from os import listdir
 import glob
 import matplotlib.pyplot as plt
-
+from PIL import Image
+from collections import OrderedDict
 '''
 20/02/18 MA
 *variable names need to be redone as they are pretty silly/meaningless at the moment
@@ -17,14 +18,19 @@ start at pixel 32 and then 33 and 34.
 It reads in a given image and then converts it to this form.
 To check that this is correct I will then convert the submission form back
 into a matrix form to check that it is correct by printing the matrix.
+
+
+ToDo: This seems to only be working with square matrices. 
 '''
+
 
 np.set_printoptions(threshold=np.nan)
 def to_form(img):
     a = np.nonzero(img) #find all the indices that are non zero in the image
-
+    #print a
     a = zip(a[1], a[0])
     a = sorted(a)
+    #print a
     a = [list(i) for i in a]
     b = [i[0]*img.shape[0]+1+i[1] for i in a]
     
@@ -55,17 +61,20 @@ def to_form(img):
 
     kk = re.sub(r'[^\w]', ' ', str(kk))      
     kk = " ".join(kk.split())
-
+   # print "type of", type(kk)
     return kk
 
 def from_formtest(subform, img):
+    print "shape of img" , np.shape(img)[1]
+    #print "subform", subform[0]
+    print type(subform)
     subform = subform.split()
  #   print subform
     k = np.array(subform)
     k= k.astype(int)
 
     masks = k.reshape([len(subform)/2,2])
-
+   # print masks
     master = []
     for i in range(len(masks)):
         sub = []
@@ -74,14 +83,19 @@ def from_formtest(subform, img):
         master.append(sub)
 
     C = [item for sublist in master for item in sublist] #very efficient list operations!
+    print "length of C", len(C)
     Z = np.repeat(1,len(C))
     er = zip(C,Z)
 
-    blank = [range(0,np.shape(img)[0] * np.shape(img)[0])] #big list! index
+    print "length of er" , len(er)
+
+    blank = [range(0,np.shape(img)[0] * np.shape(img)[1])] #big list! index
     blank = [item for sublist in blank for item in sublist]
 
-    zilch = [np.repeat(0, np.shape(img)[0] * np.shape(img)[0] )] #zeros
+    zilch = [np.repeat(0, np.shape(img)[0] * np.shape(img)[1] )] #zeros
     zilch = [item for sublist in zilch for item in sublist]
+    
+    
 
     dds = zip(blank, zilch )
 
@@ -92,20 +106,150 @@ def from_formtest(subform, img):
     out_tup = [item for item in er if item[1] not in dds[0] ] #outtup is a list of each pixel from sub form and a 1
 
     countit=0
-    for i in er: 
-        dds[i[0]-1][1] = 255
-        countit += 1
+    for i in er:
+        try:
+            dds[i[0]-1][1] = 255
+            countit += 1
+        except IndexError:
+            print "trying to access", i
 
     pixelvals = [index[1] for index in dds]
+    print pixelvals[1000:2000]
 
-    pixel_shape = np.reshape(pixelvals, [np.shape(img)[0],np.shape(img)[0]])
-    test = pixel_shape.T
+
+    pixel_shape = np.reshape(pixelvals, [np.shape(img)[1],np.shape(img)[0]])
+ #  test = pixel_shape.T
+    test = pixel_shape
     return test
 
+def from_formtest_nonsquare(subform, img):
+    print "shape of img" , np.shape(img)[0]
+    print "shape of img" , np.shape(img)[1]
+
+    #print "total number of pixels" , np.shape(img)[0] * np.shape(img)[1]
+
+    pix_count = np.shape(img)[0] * np.shape(img)[1]
+
+    print "total number of pixels" , pix_count
+
+
+    #print "subform", subform[0]
+    print type(subform)
+
+   # print "what is read in", subform
+
+    
+    #print subform
+    subform = subform.split()
+    #print subform
+    k = np.array(subform)
+    k= k.astype(int)
+    #print np.shape(k)[0]
+ #   print np.shape(k)
+
+    masks = k.reshape([len(subform)/2,2])
+   # print masks
+   # print masks
+    master = []
+    for i in range(len(masks)):
+        sub = []
+        for j in range(masks[i][1]):
+            sub.append(masks[i][0] + j)
+        master.append(sub)
+
+    master = sorted(master)
+    print len(master)
+    #Try to remove possible duplicates but that really should be a problem
+ #   ab = list(set(master))    
+#    print len(ab)
+    
+    #print "master",master[1]
+
+    C = [item for sublist in master for item in sublist] #very efficient list operations!
+    print "length of C", len(C)
+    Z = np.repeat(1,len(C))
+    er = zip(C,Z)
+
+ #  print er[1:20]
+
+ #  print "length of er" , len(er)
+
+    blank = [range(0,pix_count)] #big list! index
+    blank = [item for sublist in blank for item in sublist]
+
+ #   print len(blank)
+
+    zilch = [np.repeat(0, pix_count )] #zeros
+    zilch = [item for sublist in zilch for item in sublist]
+
+    print "length of zilch" , len(zilch)
+
+    dds = zip(blank, zilch )
+ #  print dds[0:10]
+    dds = [list(elem) for elem in dds]
+ #   print "dds" , dds[0:10]
+    er = [list(elem) for elem in er] #tuples to lists
+    pwq = [list(e) for e in zip([blank,zilch])] #big list of 256*256 values all zero now
+
+    out_tup = [item for item in er if item[1] not in dds[0] ] #outtup is a list of each pixel from sub form and a 1
+
+
+ #   print er[1:20]
+
+
+    countit=0
+    for i in er:
+        try:
+            #print i
+            dds[i[0]-1][1] = 255  #dds is a list of lists each with an index from 0 to last pixel
+
+            #print "this is i", i[0]-1            
+    
+            countit += 1
+        except IndexError:
+            print "trying to access", i
+
+    print "DDS", dds[62785:62790]
+    pixelvals = [index[1] for index in dds]
+
+ #   print "pixelvals" , pixelvals[62785:62790]
+    print "pixelvals", len(pixelvals)
+
+    print "pplease be it!"
+    print np.shape(img)[0]
+    print np.shape(img)[1]
+    pixel_shape = np.reshape(pixelvals, [np.shape(img)[1],np.shape(img)[0]])
+    test = pixel_shape
+
+ #   print test
+#    test = pixel_shape
+
+   # print "TEST", np.nonzero(test)
+
+
+    return test.T
+
 def plot_test(test, real):
+    
     print "are plots equal", np.array_equal(test,real)
     print np.count_nonzero(test)
     print np.count_nonzero(real)
+
+    print "test",   np.nonzero(test)[0][0]
+    print "test",   np.nonzero(test)[1][0]
+
+    print "real",   np.nonzero(real)[0][0]
+    print "real",   np.nonzero(real)[1][0]
+
+    #print "shape of test" , np.shape(test)[0]
+    #print "shape of test" , np.shape(test)[1]
+
+    #print "shape of real" , np.shape(real)[0]
+    #print "shape of real" , np.shape(real)[1]
+
+    #print "test",   np.nonzero(test)
+   # print "real",   np.nonzero(real)
+
 
     f = plt.figure()
     f.add_subplot(1,2,1)
@@ -117,19 +261,59 @@ def plot_test(test, real):
     plt.gray()
     plt.show(block=True)
 
+
+def mask_flatten(dir):
+    image_paths = [os.path.join(dir, x) for x in os.listdir(dir) if x.endswith('.png')]
+    pixelbank = []
+    for img in image_paths:
+        img = cv2.imread(img,0) #the image read in is a grayscale image
+ #       plot_test(img,img)
+        
+        tmp = to_form(img)
+        pixelbank.append(tmp)
+ #       pixelbank = unlist(pixelbank)
+
+    pixelbank = str(pixelbank)[1:-1]
+    pixelbank = pixelbank.replace(',', '')
+    pixelbank = pixelbank.replace("'", '')
+    return pixelbank
+        
+    
+
 def main():
     os.chdir("/Users/michaelargyrides/Documents/datascience_nuclei/stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks")
-    img = cv2.imread('f61af9040d085e725fafe93e102ee343bf8528651e8ad2cbc23487984db16e01.png',0) #the image read in is a grayscale image
+    img_path = "/Users/michaelargyrides/Documents/datascience_nuclei/stage1_train/0acd2c223d300ea55d0546797713851e818e5c697d073b7f4091b96ce0f3d2fe/masks"
+    image_paths = [os.path.join(img_path, x) for x in os.listdir(img_path) if x.endswith('.png')]
+    img = cv2.imread(image_paths[0],0)
 
-    subform = to_form(img)
-    print subform
-    coolmat = from_formtest(subform, img)
+
+#    print img_path
+ #   print image_paths
+    
+    a = mask_flatten(img_path)
+
+ #   print "a", a
+
+    coolmat = from_formtest_nonsquare(a, img)
+
+
+   # print np.array_equal(img,coolmat)
+
 
     plot_test(coolmat, img)
 
 
-#if __name__ == '__main__':
- #   main()
+    os.chdir("..")
+    fig=plt.figure()
+    ax=fig.add_subplot(1,1,1)
+    plt.axis('off')
+    plt.imshow(coolmat)
+    extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    plt.savefig(img_path+'_total.png', bbox_inches=extent)
+#    plt.savefig(img_path+"master.png", frameon=False)
+
+if __name__ == '__main__':
+    main()
 
 
 
